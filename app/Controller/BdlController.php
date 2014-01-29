bc13e43b93ebd5c7b76a197ee80864db4fe06b9a
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-01-29 15:58:55 +0100

    BDL

---
+++ app/Controller/BdlController.php
@@ -0,0 +1,35 @@
+<?
+
+class BdlController extends AppController
+{
+
+    public function dataForDimmesions()
+    {
+
+        $data = array();
+        $dims = isset( $this->request->query['dims'] ) ? $this->request->query['dims'] : array();
+        
+        App::import('model', 'DB');
+        $this->DB = new DB();
+        
+        foreach( $dims as $dim )
+        {
+	        
+	        $db_params = array();
+	        for( $i=0; $i<5; $i++ )
+	        	$db_params[ $i ] = isset( $dim[ $i ] ) ? $dim[ $i ] : 0;
+	        
+	        $db_data = $this->DB->selectAssoc("SELECT id, jednostka, ly, lv, ply, dv FROM `BDL_wymiary_kombinacje` WHERE `w1` = '" . addslashes( $db_params[0] ) . "' AND `w2` = '" . addslashes( $db_params[1] ) . "' AND `w3` = '" . addslashes( $db_params[2] ) . "' AND `w4` = '" . addslashes( $db_params[3] ) . "' AND `w5` = '" . addslashes( $db_params[4] ) . "' LIMIT 1");	        	
+	        	
+	        $data[] = array_merge($db_data, array(
+	        	'dim_str' => implode(',', $db_params),
+	        ));
+	        		        
+        }
+        
+        $this->set('data', $data);
+        $this->set('_serialize', array('data'));
+
+    }
+
+}

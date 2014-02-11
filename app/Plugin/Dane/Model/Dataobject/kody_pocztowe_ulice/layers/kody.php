837e28e00cb309ce5ee64890db9fdc0f931517db
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-02-11 15:31:57 +0100

    Kody Pocztowe

--- app/Plugin/Dane/Model/Dataobject/kody_pocztowe_ulice/layers/kody.php
+++ app/Plugin/Dane/Model/Dataobject/kody_pocztowe_ulice/layers/kody.php
@@ -1,2 +1,2 @@
 <?
-	return $this->DB->selectAssocs("SELECT `kod_id`, `kod`, `ulica` FROM `_kody_pocztowe_pna` WHERE `ulica_id`='$id'");
+	return $this->DB->selectAssocs("SELECT `kod_id`, `kod`, `ulica`, `numery` FROM `_kody_pocztowe_pna` WHERE `ulica_id`='$id'");

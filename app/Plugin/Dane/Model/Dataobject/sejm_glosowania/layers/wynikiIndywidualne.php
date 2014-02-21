1156486f60a680dbe7ed8c5f2f6429f1c8bc2b64
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-02-21 14:08:49 +0100

    Wyniki indywidualne w głosowaniach sejmowych

--- app/Plugin/Dane/Model/Dataobject/sejm_glosowania/layers/wynikiIndywidualne.php
+++ app/Plugin/Dane/Model/Dataobject/sejm_glosowania/layers/wynikiIndywidualne.php
@@ -1,8 +1,9 @@
 <?
 
-	$data = $this->DB->query("SELECT `glosy`.`glos_id`, `glosy`.`glos_str`, `poslowie`.`id`, `poslowie`.`nazwa`, `kluby`.`id`, `kluby`.`nazwa`, `kluby`.`skrot`
+	$data = $this->DB->query("SELECT `glosy`.`glos_id`, `glosy`.`glos_str`, `poslowie`.`id`, `poslowie`.`nazwa`, `kluby`.`id`, `kluby`.`nazwa`, `kluby`.`skrot`, `mowcy`.`mowca_id`
 	FROM `s_glosy` as `glosy` 
 	JOIN `s_poslowie_kadencje` as `poslowie` ON `glosy`.`posel_id` = `poslowie`.`id` 
+	JOIN `mowcy_poslowie` as `mowcy` ON `poslowie`.`id` = `mowcy`.`posel_id` 
 	JOIN `s_kluby` as `kluby` ON `glosy`.`klub_id` = `kluby`.`id` 
 	WHERE `glosy`.`glosowanie_id` = '" . $id . "' 
 	ORDER BY `poslowie`.`nazwa_rev` ASC");

0a875d58ca4e9b69b1f886f14c28c0d95cf16428
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-02-24 16:00:51 +0100

    legislacja_projekty_uchwal

--- app/Plugin/Dane/Model/Dataobject/legislacja_projekty_uchwal/layers/related.php
+++ app/Plugin/Dane/Model/Dataobject/legislacja_projekty_uchwal/layers/related.php
@@ -8,28 +8,11 @@ $output = array(
 );
 
 $objects = array(
-    'rcl' => array(),
     'sejm' => array(),
-    'prawo' => array(),
 );
 
 $projekt_id = $object['data']['id'];
 
-
-foreach ($this->DB->selectValues("SELECT id FROM rcl_katalogi WHERE akcept='1' AND katalog_id=0 AND s_projekt_id='" . $projekt_id . "'") as $pid)
-    $objects['rcl'][] = array(
-        'dataset' => 'rcl_etapy',
-        'object_template' => 'projekt',
-        'object_id' => $pid,
-    );
-
-
-foreach ($this->DB->selectValues("SELECT id FROM prawo_ustawy WHERE projekt_id='" . $projekt_id . "'") as $pid)
-    $objects['prawo'][] = array(
-        'dataset' => 'prawo',
-        'object_id' => $pid,
-        'object_template' => 'projekt',
-    );
 
 
 $q = "SELECT s_projekty_tablice_.id, s_projekty_tablice_.typ_id, s_projekty_tablice_.c_id FROM s_projekty_tablice_ LEFT JOIN s_projekty_tablice_ntypy ON s_projekty_tablice_.ntyp_id=s_projekty_tablice_ntypy.id WHERE s_projekty_tablice_.deleted='0' AND s_projekty_tablice_.projekt_id='" . $projekt_id . "' ORDER BY s_projekty_tablice_.`date` ASC, s_projekty_tablice_ntypy.ord ASC";
@@ -64,12 +47,6 @@ foreach ($this->DB->selectAssocs($q) as $item) {
 }
 
 
-if (!empty($objects['rcl']))
-    $output['groups'][] = array(
-        'id' => 'rcl',
-        'title' => 'Prace w rządzie',
-        'objects' => $objects['rcl'],
-    );
 
 if (!empty($objects['sejm']))
     $output['groups'][] = array(
@@ -78,12 +55,6 @@ if (!empty($objects['sejm']))
         'objects' => $objects['sejm'],
     );
 
-if (!empty($objects['prawo']))
-    $output['groups'][] = array(
-        'id' => 'prawo',
-        'title' => 'Opublikowana ustawa',
-        'objects' => $objects['prawo'],
-    );
 
 
 return $output;

71be79cef39c5d7e9af9516908cc0dfbab17dccf
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-04-04 22:18:05 +0200

    Groups in Powiadomienia Plugin

--- app/Plugin/Powiadomienia/Config/routes.php
+++ app/Plugin/Powiadomienia/Config/routes.php
@@ -3,6 +3,12 @@ Router::connect('/powiadomienia/phrases', array('plugin' => 'Powiadomienia', 'co
 Router::connect('/powiadomienia/phrases', array('plugin' => 'Powiadomienia', 'controller' => 'userPhrases', 'action' => 'add', '[method]' => 'POST'));
 Router::connect('/powiadomienia/phrases/:phrase_id', array('plugin' => 'Powiadomienia', 'controller' => 'userPhrases', 'action' => 'remove', '[method]' => 'DELETE'));
 
+
+Router::connect('/powiadomienia/groups', array('plugin' => 'Powiadomienia', 'controller' => 'PowiadomieniaGroups', 'action' => 'index', '[method]' => 'GET'));
+Router::connect('/powiadomienia/groups', array('plugin' => 'Powiadomienia', 'controller' => 'PowiadomieniaGroups', 'action' => 'add', '[method]' => 'POST'));
+Router::connect('/powiadomienia/groups/:phrase_id', array('plugin' => 'Powiadomienia', 'controller' => 'PowiadomieniaGroups', 'action' => 'remove', '[method]' => 'DELETE'));
+
+
 Router::connect('/powiadomienia/objects', array('plugin' => 'Powiadomienia', 'controller' => 'alertobjects', 'action' => 'index'));
 Router::connect('/powiadomienia/objects', array('plugin' => 'Powiadomienia', 'controller' => 'alertobjects', 'action' => 'flag_objects', '[method]' => 'PUT'));
 Router::connect('/powiadomienia/objects/:object_id', array('plugin' => 'Powiadomienia', 'controller' => 'alertobjects', 'action' => 'flag_object', '[method]' => 'PUT'));
---
+++ app/Plugin/Powiadomienia/Model/PowiadomieniaGroup.php
@@ -0,0 +1,29 @@
+<?php
+
+class PowiadomieniaGroup extends AppModel
+{
+    public $useTable = 'm_alerts_groups';
+    
+    public $defaultFields = array('id', 'title', 'slug', 'user_id', 'unread_count');
+    
+    public $order = array("PowiadomieniaGroup.ord" => "asc");
+    
+    public $_schema = array(
+	    'id',
+	);  
+    
+    // public $belongsTo = array('UserPhrase' => array('foreignKey' => 'alert_id', 'className' => 'Powiadomienia.UserPhrase'));
+    
+    public $paginate = array(
+        'limit' => 50,
+    );
+    
+    public function find($type = 'first', $query = array())
+    {
+	    if( !isset($query['fields']) || empty($query['fields']) )
+	    	$query['fields'] = $this->defaultFields;
+	    
+	    return parent::find($type, $query);
+	    
+    }
+} 

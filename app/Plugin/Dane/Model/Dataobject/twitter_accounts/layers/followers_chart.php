74cab2c33157760e463118eab7a485548dfdb154
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-05-06 11:16:22 +0200

    Twitter accounts

--- app/Plugin/Dane/Model/Dataobject/twitter_accounts/layers/followers_chart.php
+++ app/Plugin/Dane/Model/Dataobject/twitter_accounts/layers/followers_chart.php
@@ -3,4 +3,6 @@
 	$id = addslashes($id);
 	$data = $this->DB->selectAssocs("SELECT date, followers_count as 'count' FROM twitter_accounts_history WHERE account_id = $id ORDER BY date DESC LIMIT 30");
 	
+	array_reverse($data);
+	
 	return $data;

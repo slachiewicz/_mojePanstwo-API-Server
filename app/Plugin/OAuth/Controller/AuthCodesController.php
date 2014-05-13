c987bcfaec786559ae86651ee2365a939450c4b7
Krzysiek Madejski <krzysztof.madejski@epf.org.pl>
2014-05-13 15:17:20 +0200

    oath - forum

--- app/Plugin/OAuth/Controller/AuthCodesController.php
+++ app/Plugin/OAuth/Controller/AuthCodesController.php
@@ -4,18 +4,20 @@ class AuthCodesController extends OAuthAppController
 {
     public function save()
     {
-        if ($this->data) {
-            $ret = $this->AuthCode->save($this->data);
+        if (!empty($this->request->query)) {
+            $ret = $this->AuthCode->save($this->request->query);
             $this->set(array(
                 'return' => $ret,
                 '_serialize' => array('return'),
             ));
+        } else {
+            throw new BadRequestException();
         }
     }
 
     public function find($type = 'all')
     {
-        $auth_codes = $this->AuthCode->find($type, $this->data);
+        $auth_codes = $this->AuthCode->find($type, $this->request->query);
         $this->set(array(
             'auth_codes' => $auth_codes,
             '_serialize' => array('auth_codes'),

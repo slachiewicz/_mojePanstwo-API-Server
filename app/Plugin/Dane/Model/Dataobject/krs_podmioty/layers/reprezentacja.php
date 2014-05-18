a0a6ecaaab496422d16ac183dabce0016599c3c9
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-05-18 23:45:52 +0200

    Wiek w KRS

--- app/Plugin/Dane/Model/Dataobject/krs_podmioty/layers/reprezentacja.php
+++ app/Plugin/Dane/Model/Dataobject/krs_podmioty/layers/reprezentacja.php
@@ -1,6 +1,6 @@
 <?
 
-$data = $this->DB->query("SELECT `krs_reprezentanci`.nazwa, `krs_reprezentanci`.imiona, `krs_reprezentanci`.funkcja, `krs_osoby`.`id`, YEAR(CURRENT_TIMESTAMP) - YEAR(krs_osoby.data_urodzenia) - (RIGHT(CURRENT_TIMESTAMP, 5) < RIGHT(krs_osoby.data_urodzenia, 5)) as `wiek`, `krs_osoby`.`privacy_level` as 'privacy' 
+$data = $this->DB->query("SELECT `krs_reprezentanci`.nazwa, `krs_reprezentanci`.imiona, `krs_reprezentanci`.funkcja, `krs_osoby`.`id`, `krs_osoby`.`data_urodzenia`, `krs_osoby`.`privacy_level`  
 		FROM `krs_reprezentanci` 
 		LEFT JOIN `krs_osoby` 
 		ON `krs_reprezentanci`.`osoba_id` = `krs_osoby`.`id` 
@@ -13,7 +13,8 @@ foreach ($data as $d) {
     $output[] = array(
         'nazwa' => _ucfirst($d['krs_reprezentanci']['nazwa'] . ' ' . $d['krs_reprezentanci']['imiona']),
         'funkcja' => _ucfirst($d['krs_reprezentanci']['funkcja']),
-        'wiek' => @$d[0]['wiek'],
+        'data_urodzenia' => $d['krs_osoby']['data_urodzenia'],
+        'privacy_level' => $d['krs_osoby']['privacy_level'],
         'osoba_id' => @$d['krs_osoby']['id'],
     );
 }

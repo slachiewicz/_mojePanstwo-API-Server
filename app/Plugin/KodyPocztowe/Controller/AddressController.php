c1edd4a0ed203803689fd57e3b7801194bdb81af
Krzysiek Madejski <krzysztof.madejski@epf.org.pl>
2014-05-19 17:26:27 +0200

    swagger

--- app/Plugin/KodyPocztowe/Controller/AddressController.php
+++ app/Plugin/KodyPocztowe/Controller/AddressController.php
@@ -1,8 +1,44 @@
 <?php
+use Swagger\Annotations as SWG;
 
+/**
+ * Mapowanie kodów pocztowych na adresy
+ *
+ * @SWG\Resource(
+ *      resourcePath="/kodyPocztowe",
+ *      basePath="http://mojepanstwo.pl/api1",
+ *      apiVersion="1.0"
+ * )
+ *
+ */
 class AddressController extends AppController
 {
-
+    /**
+     * Indexo
+     *
+     * @SWG\Api(
+     *   basePath="http://mojepanstwo.pl/api2/",
+     *   path="/kodyPocztowe/cities/{city_id}/addresses/",
+     *   description="Kody pocztowe",
+     *   @SWG\Operation(
+     *      method = "GET",
+     *      summary = "Znajdź kod pocztowy na podstawie adresu",
+     *      type = "",
+     *      nickname = "address2code",
+     *
+     *      @SWG\Parameter(
+     *           name="city_id",
+     *           description="ID of pet that needs to be fetched",
+     *           paramType="path",
+     *           required=true,
+     *           type="string"
+     *         ),
+     *      @SWG\ResponseMessage(code=404, message="Pet not found"),
+     *      @SWG\ResponseMessage(code=422, message="Validation failed", responseModel="ErrorModel")
+     *   )
+     * )
+     *
+     */
     public function index()
     {
 

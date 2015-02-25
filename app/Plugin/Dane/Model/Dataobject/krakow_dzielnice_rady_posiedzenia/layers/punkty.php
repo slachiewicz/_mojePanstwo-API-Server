17cb74bf064e7b5e6e1e56d312d01382967ab52c
danielmacyszyn <daniel.macyszyn@epf.org.pl>
2015-02-25 08:53:05 +0100

    posiedzenia rad dzielnic

--- app/Plugin/Dane/Model/Dataobject/krakow_dzielnice_rady_posiedzenia/layers/punkty.php
+++ app/Plugin/Dane/Model/Dataobject/krakow_dzielnice_rady_posiedzenia/layers/punkty.php
@@ -7,10 +7,12 @@ $posiedzenie_rady = $this->DB->selectAssoc("
     WHERE id = $id
 ");
 
-$posiedzenie = $this->DB->selectAssoc("
+if( $posiedzenie = $this->DB->selectAssoc("
     SELECT id
     FROM rady_dzielnice_posiedzenia
     WHERE `date` = '".$posiedzenie_rady['data']."' AND dzielnica_id = ".$posiedzenie_rady['dzielnica_id']."
-");
+") ) {
 
-return $this->DB->selectAssocs("SELECT id, tytul as `mowca_str`, video_start FROM rady_dzielnice_posiedzenia_punkty WHERE posiedzenie_id = ".$posiedzenie['id']." AND deleted='0'");
+return $this->DB->selectAssocs("SELECT id, tytul as `mowca_str`, video_start FROM rady_dzielnice_posiedzenia_punkty WHERE posiedzenie_id = '".$posiedzenie['id']."' AND deleted='0'");
+
+} else return false;

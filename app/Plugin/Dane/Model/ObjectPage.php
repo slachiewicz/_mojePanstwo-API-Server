e7520513a1dfd41c8b61ce645ba2c70e1ef9475c
MarekBielecki <marekbielecki93@gmail.com>
2015-06-30 14:51:12 +0200

    object page cover credits

--- app/Plugin/Dane/Model/ObjectPage.php
+++ app/Plugin/Dane/Model/ObjectPage.php
@@ -14,11 +14,11 @@ class ObjectPage extends AppModel {
         $this->setLogoOrCover('logo', $value);
     }
 
-    public function setCover($value) {
-        $this->setLogoOrCover('cover', $value);
+    public function setCover($value, $credits = null) {
+        $this->setLogoOrCover('cover', $value, $credits);
     }
 
-    private function setLogoOrCover($name, $value) {
+    private function setLogoOrCover($name, $value, $credits = null) {
         $conditions = array(
             'ObjectPage.dataset' => $this->request['dataset'],
             'ObjectPage.object_id' => (int) $this->request['object_id']
@@ -40,17 +40,28 @@ class ObjectPage extends AppModel {
             if($remove) {
                 $this->deleteAll($conditions, false);
             } else {
-                $this->updateAll(array(
+                $data = array(
                     $name => $value ? '1' : '0'
-                ), $conditions);
+                );
+
+                if($credits != null)
+                    $data['cover_credits'] = $credits;
+
+                $this->updateAll($data, $conditions);
             }
         } else {
+
+            $data = array(
+                'dataset' => $this->request['dataset'],
+                'object_id' => (int) $this->request['object_id'],
+                $name => $value ? '1': '0',
+            );
+
+            if($credits != null)
+                $data['cover_credits'] = $credits;
+
             $this->save(array(
-                'ObjectPage' => array(
-                    'dataset' => $this->request['dataset'],
-                    'object_id' => (int) $this->request['object_id'],
-                    $name => $value ? '1': '0',
-                )
+                'ObjectPage' => $data
             ));
         }
     }

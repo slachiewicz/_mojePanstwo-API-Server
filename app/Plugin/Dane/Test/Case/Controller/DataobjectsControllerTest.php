686792fc8296a314acb7264b41474451746de4b9
Krzysiek <kangoor@skrzynka.pl>
2015-07-07 13:25:18 +0200

    Tests for HATEOS #18

--- app/Plugin/Dane/Test/Case/Controller/DataobjectsControllerTest.php
+++ app/Plugin/Dane/Test/Case/Controller/DataobjectsControllerTest.php
@@ -50,7 +50,7 @@ class DataobjectsControllerTest extends ControllerTestCase {
         );
     }
 
-    private function expectFind($params, $response, $count) {
+    private function dataobjectExpectsFind($params, $response, $count) {
         $this->controller->Dataobject->expects($this->once())
             ->method('find')->with($this->equalTo('all'), $this->equalTo($params))
             ->will($this->returnValue($response));
@@ -65,7 +65,7 @@ class DataobjectsControllerTest extends ControllerTestCase {
     }
 
     public function testIndexSimple() {
-        $this->expectFind(array(
+        $this->dataobjectExpectsFind(array(
             'conditions' => array(
                 'dataset' => 'wojewodztwa'
             )
@@ -73,15 +73,141 @@ class DataobjectsControllerTest extends ControllerTestCase {
 
         $this->testAction('/dane/wojewodztwa');
 
-        $this->assertEquals(array(
+        $this->assertSame(array(
             '_serialize' => array('_items', '_meta'),
             '_items' => $this->defaultResponse,
             '_meta' => array('page' => 1, 'max_results' => MPSearch::RESULTS_COUNT_MAX, 'total' => 3)
         ), $this->vars);
     }
 
+    public function assertArraySameValues($expected, $actual) {
+        $this->assertSame(array_diff($expected, $actual), array_diff($actual, $expected));
+    }
 
-    public function testIndexHATEOAS() {
-        // TODO
+    public function assertUrlSamePathAndQuery($expected, $actual) {
+        $this->assertUrlSame($expected, $actual, array('path', 'query'));
+    }
+
+    /**
+     * @see http://php.net/manual/en/function.parse-url.php
+     * @param $expected
+     * @param $actual
+     * @param $fields
+     */
+    public function assertUrlSame($expected, $actual, $fields =
+    array('host', 'path', 'query', 'fragment', 'scheme', 'port', 'user', 'pass' )) {
+        $expected = parse_url($expected);
+        $actual = parse_url($actual);
+
+        foreach($fields as $fld) {
+            $this->assertSame($expected[$fld], $actual[$fld]);
+        }
+    }
+
+    public function assertArrayMatchingKeysSame($expected, $actual) {
+        $this->assertSame(array_intersect_key($expected, $actual), array_intersect_key($actual, $expected));
+    }
+
+    public function assertArrayMatchingKeysNotSame($expected, $actual) {
+        $this->assertNotSame(array_intersect_key($expected, $actual), array_intersect_key($actual, $expected));
+    }
+
+    public function testAssertArrayMatchingKeysSame() {
+        $this->assertArrayMatchingKeysSame(
+            array('k1' => 'v1', 'k2' => 'v2'),
+            array('k2' => 'v2', 'k3' => 'v1')
+        );
+
+        // no matching keys is an empty-empty match
+        $this->assertArrayMatchingKeysSame(
+            array('k1' => 'v1', 'k2' => 'v2'),
+            array('k4' => 'v2', 'k3' => 'v1')
+        );
+    }
+
+    public function testAssertArrayMatchingKeysNotEquals() {
+        $this->assertArrayMatchingKeysNotSame(
+            array('k1' => 'v1', 'k2' => 'v2'),
+            array('k2' => 'v3', 'k3' => 'v1')
+        );
+    }
+
+    public function testAssertUrlEqualsQueryNoOrder() {
+        $this->assertUrlSame('?val1=1&val2=2', '?val2=2&val1=1');
+    }
+
+    public function testIndexHateoasFirst() {
+        $this->dataobjectExpectsFind(array(
+            'conditions' => array(
+                'dataset' => 'wojewodztwa'
+            ),
+            'limit' => 1
+        ), array($this->defaultResponse[0]), 3);
+
+        $this->testAction('/dane/wojewodztwa?limit=1');
+
+
+        $this->assertArrayMatchingKeysSame(array(
+            '_items' => array($this->defaultResponse[0]),
+            '_meta' => array('page' => 1, 'max_results' => MPSearch::RESULTS_COUNT_MAX, 'total' => 3),
+        ), $this->vars);
+
+        $links = $this->vars['_links'];
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1', $links['self']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=3', $links['last']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=2', $links['next']);
+
+        $this->assertArrayNotHasKey('first', $links);
+        $this->assertArrayNotHasKey('prev', $links);
+    }
+
+    public function testIndexHateoasMiddle() {
+        $this->dataobjectExpectsFind(array(
+            'conditions' => array(
+                'dataset' => 'wojewodztwa'
+            ),
+            'limit' => 1,
+            'page' => 2
+        ), array($this->defaultResponse[0]), 3);
+
+        $this->testAction('/dane/wojewodztwa?limit=1&page=2');
+
+        $this->assertArrayMatchingKeysSame(array(
+            '_items' => array($this->defaultResponse[0]),
+            '_meta' => array('page' => 2, 'max_results' => MPSearch::RESULTS_COUNT_MAX, 'total' => 3),
+        ), $this->vars);
+
+        $links = $this->vars['_links'];
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=2', $links['self']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=3', $links['last']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=3', $links['next']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=1', $links['prev']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=1', $links['first']);
+    }
+
+    public function testIndexHateoasLast() {
+        $this->dataobjectExpectsFind(array(
+            'conditions' => array(
+                'dataset' => 'wojewodztwa'
+            ),
+            'limit' => 1,
+            'page' => 3
+        ), array($this->defaultResponse[0]), 3);
+
+        $this->testAction('/dane/wojewodztwa?limit=1&page=3');
+
+
+        $this->assertArrayMatchingKeysSame(array(
+            '_items' => array($this->defaultResponse[0]),
+            '_meta' => array('page' => 3, 'max_results' => MPSearch::RESULTS_COUNT_MAX, 'total' => 3),
+        ), $this->vars);
+
+        $links = $this->vars['_links'];
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=3', $links['self']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=1', $links['first']);
+        $this->assertUrlSamePathAndQuery('/dane/wojewodztwa?limit=1&page=2', $links['prev']);
+
+        $this->assertArrayNotHasKey('last', $links);
+        $this->assertArrayNotHasKey('next', $links);
     }
 }

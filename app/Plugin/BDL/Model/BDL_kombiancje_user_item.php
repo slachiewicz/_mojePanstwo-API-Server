ad8056f2a6080d98cf58a5eb41bfa9aa4b70c989
McCease <maxiz@wp.pl>
2015-07-16 10:52:31 +0200

    API BDLTempItems

---
+++ app/Plugin/BDL/Model/BDL_kombiancje_user_item.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tomaszdrazewski
+ * Date: 16/07/15
+ * Time: 10:15
+ */
+class BDL_kombiancje_user_item extends AppModel
+{
+    public $useTable = 'BDL_kombiancje_user_item';
+}
--- app/Plugin/BDL/Model/BdlTempItem.php
+++ app/Plugin/BDL/Model/BdlTempItem.php
@@ -3,100 +3,18 @@
 class BdlTempItem extends AppModel
 {
 
-    public $useTable = false;
+    public $useTable = 'panel_wskazniki';
 
-    public function search($type = 'first', $params = array())
-    {
-        $ret = null;
-        if (!CakeSession::check('TempItems')) {
-            return false;
-        }
-
-        $ret = CakeSession::read('TempItems');
-
-        if (isset($params['conditions'])) {
-            if (isset($params['conditions']['id'])) {
-                $id = $params['conditions']['id'];
-                if ($id == 0) {
-                    $ret = Set::classicExtract($ret, "$id");
-                } else {
-                    $ret = array(Set::classicExtract($ret, "$id"));
-                }
-            }
-        }
-
-        if (isset($params['order'])) {
-            if ($params['order'] == 'DESC') {
-                $ret = array_reverse($ret);
-            }
-        }
-
-        if ($ret == null) {
-            return false;
-        }
-
-        switch ($type) {
-            case 'first': {
-                return $ret[0];
-                break;
-            }
-            case 'all': {
-                return $ret;
-                break;
-            }
-            case 'list': {
-                $ret2 = array();
-               // debug($ret);
-                foreach ($ret as $key => $val) {
-                    $ret2[$key]=$val['tytul'];
-                }
-                return $ret2;
-                break;
-            }
-            case 'count': {
-                return count($ret);
-                break;
-            }
-        }
-
-        return false;
-    }
-
-    public function searchById($id)
-    {
-        return $this->search('first', array('conditions' => array('id' => $id)));
-    }
-
-    public function save($data)
-    {
-        if (isset($data['id'])) {
-            if (CakeSession::write('TempItems.'.$data['id'], $data)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        if (CakeSession::check('TempItems')) {
-            $ret = CakeSession::read('TempItems');
-            array_push($ret, $data);
-        } else {
-            $ret = array($data);
-        }
-        if (CakeSession::write('TempItems', $ret)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function delete($id)
-    {
-        if (CakeSession::delete('TempItems.' . $id)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    public $hasAndBelongsToMany = array(
+        'WymiaryKombinacje' =>
+            array(
+                'className' => 'BDL.WymiaryKombinacje',
+                'joinTable' => 'BDL_kombiancje_user_item',
+                'with' => 'BDL.BDL_kombiancje_user_item',
+                'foreignKey' => 'useritem_id',
+                'associationForeignKey' => 'dim_id',
+            )
+    );
+    public $actsAs = array('Containable');
 
 }

121319d34dd6a81661ab69d899a9c279386c0c76
McCease <maxiz@wp.pl>
2015-08-04 17:04:52 +0200

    PK - glosy - autoryzacja

--- app/Plugin/Krakow/Controller/GlosyController.php
+++ app/Plugin/Krakow/Controller/GlosyController.php
@@ -1,17 +1,31 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: tomaszdrazewski
  * Date: 04/08/15
  * Time: 16:07
  */
-
 class GlosyController extends AppController
 {
 
     public $uses = array('Krakow.UserVotes');
 
-    public function save(){
+
+    public function beforeFilter()
+    {
+        parent::beforeFilter();
+
+        if ($this->request->query['apiKey'] !== ROOT_API_KEY) {
+            // deny access to Paszport from untrusted clients
+            throw new ForbiddenException();
+        }
+        // deny all unauthenticated if not explicitly allowed
+        $this->Auth->deny();
+    }
+
+    public function save()
+    {
         $this->UserVotes->create();
         if ($this->UserVotes->save($this->request->data)) {
             $message = 1;

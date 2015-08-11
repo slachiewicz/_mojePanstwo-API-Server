b1b18eeeb27c6efabb0e3676132c4f04eee0bce1
MarekBielecki <marekbielecki93@gmail.com>
2015-08-11 17:11:57 +0200

    radni gmin okreg new layer

--- app/Plugin/Dane/Model/Dataobject/radni_gmin/layers/okreg.php
+++ app/Plugin/Dane/Model/Dataobject/radni_gmin/layers/okreg.php
@@ -1,7 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11.08.15
- * Time: 16:30
- */
+<?
+
+return $this->DB->selectRow("SELECT okregi.id, okregi.rok, okregi.nr_okregu, AsText(okregi.polygon), okregi.dzielnice, okregi.ilosc_mieszkancow, okregi.liczba_mandatow, okregi.ilosc_miesz_norma_przedst FROM pl_gminy_radni JOIN pl_gminy_krakow_okregi as okregi ON okregi.okreg_id = pl_gminy_radni.okreg_id WHERE pl_gminy_radni.id = " . addslashes( $id ));
+

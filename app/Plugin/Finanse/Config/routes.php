62bfbcb5afd5c5ef6c99b2a43e40e4e0ee0be3d2
MarekBielecki <marekbielecki93@gmail.com>
2015-08-17 17:58:26 +0200

    communes budget

--- app/Plugin/Finanse/Config/routes.php
+++ app/Plugin/Finanse/Config/routes.php
@@ -1,2 +1,24 @@
 <?
+
 Router::connect('/finanse/:action', array('plugin' => 'Finanse', 'controller' => 'Finanse'));
+
+Router::connect('/finanse/gminy/:commune_id/budzet/:type/dzialy/:id', array(
+    'plugin' => 'Finanse',
+    'controller' => 'Communes',
+    'action' => 'section'
+), array(
+    'id' => '([0-9]+)',
+    'commune_id' => '([0-9]+)',
+    'type' => '(wydatki|dochody)',
+    'pass' => array('id', 'commune_id', 'type')
+));
+
+Router::connect('/finanse/gminy/:commune_id/budzet/:type/dzialy', array(
+    'plugin' => 'Finanse',
+    'controller' => 'Communes',
+    'action' => 'sections'
+), array(
+    'commune_id' => '([0-9]+)',
+    'type' => '(wydatki|dochody)',
+    'pass' => array('commune_id', 'type')
+));

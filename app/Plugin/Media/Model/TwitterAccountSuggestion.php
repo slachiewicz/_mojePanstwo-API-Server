348d4cf4da3424ff542445c214af5a8b6b093dcc
MarekBielecki <marekbielecki93@gmail.com>
2015-08-25 16:40:59 +0200

    twitter account suggestion url validation

--- app/Plugin/Media/Model/TwitterAccountSuggestion.php
+++ app/Plugin/Media/Model/TwitterAccountSuggestion.php
@@ -17,9 +17,14 @@ class TwitterAccountSuggestion extends AppModel {
         if(!in_array($row['type_id'], $this->types))
             throw new Exception('Nieprawidłowy typ konta');
 
-        $data['name'] = trim(trim($data['name'], '@'));
+        $data['name'] = trim($data['name']);
         if(!strlen($data['name']))
             throw new Exception('Nazwa konta nie może być pusta');
+
+        if(preg_match("|https?://(www\.)?twitter\.com/(#!/)?@?([^/]*)|", $data['name'], $matches)) {
+            $row['name'] = $matches[3];
+        } else
+            throw new Exception('Nieprawidłowy link do profilu');
 
         return $this->save($row);
     }

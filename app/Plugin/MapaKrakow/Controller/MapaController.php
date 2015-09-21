63d7adaf5c9712fb8af5a31ae019dcd950799777
McCease <maxiz@wp.pl>
2015-09-21 09:54:23 +0200

    Mapa - krakow - minor fix

--- app/Plugin/MapaKrakow/Controller/MapaController.php
+++ app/Plugin/MapaKrakow/Controller/MapaController.php
@@ -8,16 +8,32 @@
  */
 class MapaController extends AppController
 {
-    public $uses=array('MapaKrakow.Dzielnice');
+    public $uses = array('MapaKrakow.Dzielnice');
 
     public function dzielnice()
     {
-       $data=$this->Dzielnice->find('all', array(
-        'fields'=>array(
-            'nazwa','numer','spat'
-        )
-    ));
+        $data = $this->Dzielnice->find('all', array(
+            'conditions' => array(
+                'layer_id' => 6,
+                //'or'=>array(array('numer'=>'X'),array('numer'=>'XIII'))
+            ),
+            'fields' => array(
+                'nazwa', 'numer', 'spat'
+            )
+        ));
+        $dane = array(
+            'type'=>'poly',
+            'layer'=>'dzielnice',
+            'dane'=>array()
+        );
+        foreach ($data as $row) {
+            $ret=array(
+                'id'=>$row['Dzielnice']['numer'],
+                'spat'=>$row['Dzielnice']['spat']
+            );
+            $dane['dane'][]=$ret;
 
-        $this->setSerialized('response', $data);
+        }
+        $this->setSerialized('response', $dane);
     }
 }
